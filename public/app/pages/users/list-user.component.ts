aa743283315f7ac46712b61a9e4e38d5606d5aa2
jorge <georgejolema@gmail.com>
2018-10-23 07:10:26 -0600

    add filtering to users list

--- public/app/pages/users/list-user.component.ts
+++ public/app/pages/users/list-user.component.ts
@@ -11,25 +11,43 @@ export class ListUserComponent implements OnInit {
     users: User[] = [];
     pages: number[] = [];
     totalPages: number;
-    page: number;
-    constructor(private apiUser: UserService, private router: Router) {
+    page = 1;
+    search = '';
 
-    }
+    constructor(private apiUser: UserService, private router: Router) {}
 
     ngOnInit() {
         this.apiUser.getUsers().subscribe( (users) => {
             this.users = users.reverse();
-            this.totalPages = Math.floor(users.length / 10);
-            if (this.totalPages % 10 !== 0) { this.totalPages++; }
-            this.page = 1;
-            this.pages = Array
-                .apply(null, new Array(this.totalPages))
-                .map((data: any, index: number) => index + 1);
+        });
+    }
+
+    private calculate(users: User[]) {
+        this.totalPages = Math.floor(users.length / 10);
+
+        if (this.totalPages % 10 !== 0) { this.totalPages++; }
+
+        if (this.page > this.totalPages) { this.page = 1; }
+
+        this.pages = Array
+            .apply(null, new Array(this.totalPages))
+            .map((data: any, index: number) => index + 1);
+        return users;
+    }
+
+    private filter() {
+        return this.users.filter((item) => {
+            if (this.search === '') { return true; }
+
+            return item.firstName.toLowerCase().indexOf(this.search) !== -1 ||
+                item.lastName.toLowerCase().indexOf(this.search) !== -1;
         });
     }
 
     getDataSet() {
-        return this.users.slice((this.page - 1) * 10, this.page * 10);
+        const users = this.filter();
+        return this.calculate(users)
+            .slice((this.page - 1) * 10, this.page * 10);
     }
 
     addUser() {
